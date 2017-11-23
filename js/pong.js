4637adeba7d02a7754fa8d6b56a35b4a366eb404
Aluno <Aluno>
2017-11-23 11:49:35 -0300

    Junção em um unico arquivo

--- js/pong.js
+++
@@ -1,9 +0,0 @@
-
-
-$(document).keypress(function(event){
-  //alert(String.fromCharCode(event.which)); 
-  if(String.fromCharCode(event.which)=='w')
-  $("#raqueteEsquerda").css({"margin-top": -15});
-});
-
-	
